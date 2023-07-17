523207614998490044c9a373b948ffea486d203a
Docta Rhyf <drrhyf@gmail.com>
2023-07-17 16:08:20 +0200

    first commit

---
+++ src/pages/PageLogin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function PageLogin(props){
+    return(
+        <section className="bg-mainBlue text-white h-screen">
+            
+        </section>
+    )
+}
