22023e553fd54852075a799d5af8e6f9266d328f
Docta Rhyf <drrhyf@gmail.com>
2023-07-18 18:04:28 +0200

    working

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -12,10 +12,23 @@ function ButtonHomeMenu(props){
     
     
     return (
-        <button className="border w-max min-w-[50%] hover:bg-koop-d hover:text-white hover:shadow-md shadow-koop-d flex space-x-2 border-koop-def px-4 py-2 bg-white rounded-xl text-black" >
+        <button className="border w-max min-w-[50%] hover:bg-koop-d hover:text-white hover:shadow-md shadow-koop-d flex items-center space-x-2 border-koop-def px-4 py-2 bg-white rounded-xl text-black" >
             <span><img className="  object-cover " width={40} height={40} src={icon} /></span>
             <span>{label}</span>
         </button>
+    )
+}
+
+function ItemTopMenu(props){
+
+    let { icon, title } = props;
+
+
+    return(
+        <div className="text-black" >
+            <img src={icon} width={32} height={32} />
+            <span>{ title }</span>
+        </div>
     )
 }
 
@@ -32,6 +45,9 @@ export default function PageHome(props){
             <div className="menu-bar flex justify-between p-2">
                 <button><img src={menu} /></button>
                 <button><img onClick={e => setShowMenu(!showMenu)} className="rounded-full" width={32} height={32} src={'https://pbs.twimg.com/profile_images/664701790537973760/PF7GvcBA_400x400.jpg'} /></button>
+                <div className={` ${ showMenu ? 'hidden' : '' } absolute right-4 min-h-[120pt] bg-white min-w-[200pt] top-[46pt] shadow-xl rounded-[18pt]`}>
+                    <ItemTopMenu icon={provserv} title='My Account' badge='0' />
+                </div>
             </div>
 
             <div className="billbord flex flex-col items-center">
