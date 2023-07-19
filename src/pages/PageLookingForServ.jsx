958c2695e24fb2c47bc41643b2bbc2fc600c1016
Docta Rhyf <drrhyf@gmail.com>
2023-07-19 16:58:55 +0200

    working on req new serv

--- src/pages/PageLookingForServ.jsx
+++ src/pages/PageLookingForServ.jsx
@@ -3,6 +3,7 @@ import * as classes from "../helpers/class";
 import provserv from '../assets/icons/provserv.png'
 import lookserv from '../assets/icons/lookserv.png'
 import otherserv from '../assets/icons/otherserv.png'
+import search from '../assets/icons/search.png'
 import reqserv from '../assets/icons/reqserv.png'
 import '../App.css'
 import MenuTopBar from "../comps/MenuTopBar";
@@ -28,17 +29,25 @@ export default function PageLookingForServ(props){
 
     const ar = new Array(8).fill(0);
 
+    function onShowServiceDetails(d){
+        console.log('onShowServiceDetails', d)
+    }
+
     return(
         <section className={` p-2 ${classes.clPage}`}>
            
             <MenuTopBar setSelectedPage={setSelectedPage} />
 
-            <div className="search-box">
-                search
+            <div className="search-box h-[24pt] m-2 mt-4 flex rounded-lg overflow-hidden">
+                
+                <input type="search" className="text-black focus:outline-none h-[24pt] px-2 flex-grow " placeholder="Search ..." />
+                <button className=" flex justify-center items-center focus:outline-none w-[24pt]  hover:bg-gray-200 h-[24pt] flex-none bg-white font-bold" >
+                    <img src={search} className="w-[14pt] h-[14pt]" />
+                </button>
 
             </div>
 
-            <div className="text-black flex flex-col gap-4 items-center  menu-cont bg-white rounded-[30pt] p-8 mt-8 mx-2">
+            <div className="shadow-xl text-black flex flex-col gap-4 items-center  menu-cont bg-white rounded-[30pt] p-8 mt-8 mx-2">
                 <ButtonHomeMenu label='Request New Service' icon={reqserv} />
                 <p>To request a new which is not listed below</p>
 
@@ -51,7 +60,10 @@ export default function PageLookingForServ(props){
                     <div className=" flex overflow-x-scroll gap-4 pb-4 rounded-lg ">
                         {
                             ar.map((it, i) => 
-                            <div className="bg-red-500 w-[150pt] h-[120pt] flex-shrink-0 rounded-lg" >Cool</div>)
+                            <button key={i} onClick={e => onShowServiceDetails(it)} >
+                                <div className="bg-red-500 w-[150pt] h-[120pt] flex-shrink-0 rounded-lg" >Cool</div>
+                            </button>
+                                )
                         }
 
 
@@ -69,7 +81,9 @@ export default function PageLookingForServ(props){
                     <div className=" flex overflow-x-scroll gap-4 pb-4 rounded-lg ">
                         {
                             ar.map((it, i) => 
-                            <div className="bg-red-500 w-[150pt] h-[120pt] flex-shrink-0 rounded-lg" >Cool</div>)
+                            <button key={i} onClick={e => onShowShopDetails(it)} >
+                                <div className="bg-red-500 w-[150pt] h-[120pt] flex-shrink-0 rounded-lg" >Cool</div>
+                            </button>)
                         }
 
 
