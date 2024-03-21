adb614a77e46031ba770908f05b921c73f7cb76e
Docta Rhyf <drrhyf@gmail.com>
2024-03-21 21:02:58 +0200

    upd

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,11 @@
+import { supabase, TABLE_NAMES } from "./supabase";
+
 const API_ENDPOINT = "https://konext.vercel.app/api";
 
 export const login = async (phone, pin) => {
   const API_LOGIN = `${API_ENDPOINT}/auth/login`;
   const loginData = { phone: phone, pin: pin };
+  let errorMessage = null;
 
   try {
     const response = await fetch(`${API_LOGIN}`, {
@@ -14,15 +17,20 @@ export const login = async (phone, pin) => {
     });
 
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      errorMessage = "Network response was not ok";
+      throw new Error(errorMessage);
     }
 
-    const data = await response.json();
-    // Handle the data received from the server
+    data = await response.json();
     console.log(data);
+    return data;
   } catch (error) {
     // Handle errors
-    console.error("There was a problem with the fetch operation:", error);
+    errorMessage = `There was a problem with the fetch operation: ${JSON.stringify(
+      error
+    )}`;
+    console.error(errorMessage);
+    return { error: true, message: errorMessage };
   }
 
   return API_LOGIN;
