bdbb4d18ff950e982467dfd4366c44170f7cb625
Docta Rhyf <drrhyf@gmail.com>
2024-03-27 10:53:03 +0200

    dbg tos link

--- screens/TOS.js
+++ screens/TOS.js
@@ -5,7 +5,7 @@ import { WebView } from "react-native-webview";
 export default function TOS() {
   return (
     <WebView
-      source={{ uri: "https://konext.vercel.app/" }}
+      source={{ uri: "https://konext.vercel.app/tos" }}
       style={{ flex: 1 }}
     />
   );
