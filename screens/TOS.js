5dafa88e41a17c0412787124a18c3f56556538ee
Docta Rhyf <drrhyf@gmail.com>
2024-03-27 10:50:32 +0200

    add tos

--- screens/TOS.js
+++ screens/TOS.js
@@ -1,40 +1,12 @@
 import React from "react";
 import { ScrollView, Text } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function TOS() {
   return (
-    <ScrollView>
-      <Text>
-        Conditions Générales de KOOP Bonjour KOOPer, Bienvenue sur KOOP,
-        l'application qui te met en lien avec des trésors cachés et des
-        opportunités de folie! Avant de plonger dans l'aventure, prends une
-        minute pour lire nos Conditions Générales d'Utilisation (les CGU").
-        Elles s'assurent une expérience sûre et amusante sur notre app. Des
-        Trouvailles Uniques : Sur KOOP, découvre des trésors rares et uniques
-        que tu ne trouveras nulle part ailleurs! De l'objet vintage au produit
-        de luxe, notre app te connecte avec des vendeurs de confiance. Connexion
-        Vendeurs-Acheteurs : Grâce à KOOP, fais partie d'une communauté où
-        vendeurs et acheteurs se rencontrent. Que tu sois à la recherche de
-        quelque chose de spécial ou que tu veuilles vendre ton propre trésor,
-        KOOP est là pour toi. Transfert d'Argent Facile : Avec KOOP, envoyer de
-        l'argent est aussi simple qu'un clic. Transfère de l'argent directement
-        depuis ta carte SIM mobile en toute sécurité. Achète et vends en toute
-        confiance, sans tracas. Suivi GPS pour Véhicules : Toujours savoir où se
-        trouve ton bolide? KOOP t'offre le suivi GPS pour tes véhicules. Que ce
-        soit pour des livraisons ou simplement pour garder un œil sur tes biens,
-        KOOP te facilite la vie. Partage de Liens et Gains : Sur KOOP, le
-        partage peut rapporter gros! Partage des liens avec tes amis et ta
-        famille et gagne des récompenses. Plus tu partages, plus tu gagnes.
-        C'est simple, non? En utilisant KOOP, tu acceptes nos CGU. Nous te
-        rappelons qu'il est strictement interdit de publier tout contenu
-        pornographique ou illégal sur notre plateforme. Nous nous réservons le
-        droit de supprimer tout contenu en violation de cette règle et de
-        prendre des mesures appropriées, y compris la résiliation de ton compte.
-        Alors, prêt à découvrir le monde de KOOP? Let's KOOP it up! Remarque :
-        Ces CGU sont là pour assurer une expérience incroyable pour tous les
-        KOOPers. En cas de questions, n'hésite pas à nous contacter. Amuse-toi
-        bien!
-      </Text>
-    </ScrollView>
+    <WebView
+      source={{ uri: "https://konext.vercel.app/" }}
+      style={{ flex: 1 }}
+    />
   );
 }
