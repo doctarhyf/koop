7d4ab6291398d55c2b181f108c76284d78b8be7b
Docta Rhyf <drrhyf@gmail.com>
2024-09-28 07:18:08 +0200

    dbg

--- screens/PhotoViewer.js
+++ screens/PhotoViewer.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React from "react";
 //import styles from "../helpers/styles";
-import { View, ScrollView, StyleSheet, Dimensions } from "react-native";
 import { Image } from "expo-image";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 
 export default function PhotoViewer({ route, navigation }) {
   const { params } = route;
@@ -39,15 +39,3 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height, //* 2, // or set a specific height
   },
 });
-
-/* return (
-    <View style={[styles.flex1, styles.bgBlack]}>
-      <Image
-        transition={1000}
-        source={params || require("../assets/koop.png")}
-        style={[styles.flex1, { width: "100%", height: "100%" }]}
-      />
-    </View>
-  );
-}
- */
